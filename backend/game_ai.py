9e3aa8d76dd27a9e958e8d0e51eff65116063c5b
yshimizu20 <yukohshimizu@icloud.com>
2022-10-26 14:40:53 -0400

    implemented search params calculations

--- backend/game_ai.py
+++ backend/game_ai.py
@@ -5,9 +5,12 @@ from board import Board
 class Policy():
   def __init__(self):
     self.game = Game()
+    self.moves = 0
 
   def run(self):
     while True:
+      self.moves += 1
+      self.misc()
       print(self.game.board)
       direction = self.best_move()
       score, is_changed = self.game.make_move(direction)
@@ -28,13 +31,16 @@ class Policy():
   def evaluate_board(self):
     raise NotImplementedError
 
+  def misc(self):
+    raise NotImplementedError
+
 
 class MonteCarloPolicy(Policy):
   def __init__(self):
     super().__init__()
 
-    self.search_per_move = 20
-    self.search_depth = 5
+    self.spm_scale_param = 20
+    self.sl_scale_param = 4
     self.search_param = 200
 
   def best_move(self):
@@ -49,11 +55,11 @@ class MonteCarloPolicy(Policy):
       new_board = board
       scores[MOVES.index(direction)] += score
       
-      for _ in range(self.search_per_move):
+      for _ in range(self.searches_per_move):
         move_iter = 1
         game_is_valid = True
 
-        while game_is_valid and move_iter < self.search_depth:
+        while game_is_valid and move_iter < self.search_length:
           new_board, score, is_changed = new_board.random_play()
           scores[MOVES.index(direction)] += score
           move_iter += 1
@@ -64,3 +70,10 @@ class MonteCarloPolicy(Policy):
 
   def evaluate_board(self):
     pass
+
+  def misc(self):
+    self.calculate_params()
+
+  def calculate_params(self):
+    self.searches_per_move = self.spm_scale_param * (1 + (self.moves // self.search_param))
+    self.search_length = self.sl_scale_param * (1 + (self.moves // self.search_param))
